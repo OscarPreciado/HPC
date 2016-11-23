f7308b8a0f43922fa7bc54ef5e0f4c00627f9e21
OscarPreciado <oapreciado@utp.edu.co>
2016-11-23 13:28:50 -0500

    Update segundoparcial.sh

--- parcial2/segundoparcial.sh
+++ parcial2/segundoparcial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --job-name=parcial2
-#SBATCH --output=parcial2.out
+#SBATCH --job-name=segundoparcial
+#SBATCH --output=segundoparcial.out
 #SBATCH --nodes=2
 #SBATCH --tasks=2
 #SBATCH --gres=gpu:1
